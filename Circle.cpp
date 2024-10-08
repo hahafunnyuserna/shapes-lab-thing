1aead1dc78f221fe6ce7c9e8778e604f4bd49fff
Nick Spizzirri <Nick.spizzirri91993@gmail.com>
2024-10-08 14:55:26 -0500

    Add Shape + Circle

---
+++ Circle.cpp
@@ -0,0 +1,9 @@
+
+class Circle{
+    public:
+    double pi =3.14;
+    double radius =0;
+    double getArea(){
+        return (pi)*(radius);
+    }   
+};
---
+++ Shape.cpp
@@ -0,0 +1,4 @@
+class Shape{
+virtual double getArea();
+virtual double getPerimeter();
+};
